4cbf998be9a550189cefcfc8f8442e277c2ee926
Riaz Arbi <riazarbi@users.noreply.github.com>
2021-09-27 20:10:53 +0200

    Update cuda_setup.sh

--- cuda_setup.sh
+++ cuda_setup.sh
@@ -2,42 +2,100 @@
 
 # Taken from here: https://gitlab.com/nvidia/cuda/blob/ubuntu16.04/10.0/base/Dockerfile
 # Base
-apt-get update && apt-get install -y --no-install-recommends gnupg2 curl ca-certificates && \
-    curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub | apt-key add - && \
-    echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list && \
-    echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/nvid$
-    apt-get purge --autoremove -y curl && \
-    rm -rf /var/lib/apt/lists/*
 
-export CUDA_VERSION=10.0.130
-export CUDA_PKG_VERSION=10-0=$CUDA_VERSION-1
+export NVARCH=x86_64
+export NVIDIA_REQUIRE_CUDA="cuda>=11.1 brand=tesla,driver>=418,driver<419 brand=tesla,driver>=440,driver<441 driver>=450"
+export NV_CUDA_CUDART_VERSION=11.1.74-1
+export NV_CUDA_COMPAT_PACKAGE=cuda-compat-11-1
+
+export NV_ML_REPO_ENABLED=1
+export NV_ML_REPO_URL=https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/${NVARCH}
+
+apt-get update && apt-get install -y --no-install-recommends \
+    gnupg2 curl ca-certificates && \
+    curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/${NVARCH}/7fa2af80.pub | apt-key add - && \
+    echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/${NVARCH} /" > /etc/apt/sources.list.d/cuda.list && \
+    if [ ! -z ${NV_ML_REPO_ENABLED} ]; then echo "deb ${NV_ML_REPO_URL} /" > /etc/apt/sources.list.d/nvidia-ml.list; fi && \
+    apt-get purge --autoremove -y curl \
+    && rm -rf /var/lib/apt/lists/*
+
+export CUDA_VERSION=11.1.1
 
 # For libraries in the cuda-compat-* package: https://docs.nvidia.com/cuda/eula/index.html#attachment-a
-apt-get update && apt-get install -y --no-install-recommends \
-        cuda-cudart-$CUDA_PKG_VERSION \
-        cuda-compat-10-0=410.48-1 && \
-    ln -s cuda-10.0 /usr/local/cuda && \
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    cuda-cudart-11-1=${NV_CUDA_CUDART_VERSION} \
+    ${NV_CUDA_COMPAT_PACKAGE} \
+    && ln -s cuda-11.1 /usr/local/cuda && \
     rm -rf /var/lib/apt/lists/*
 
-export PATH=/usr/local/cuda/bin:${PATH}
+# Required for nvidia-docker v1
+RUN echo "/usr/local/nvidia/lib" >> /etc/ld.so.conf.d/nvidia.conf \
+    && echo "/usr/local/nvidia/lib64" >> /etc/ld.so.conf.d/nvidia.conf
+
+export PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64
+
+COPY NGC-DL-CONTAINER-LICENSE /
 
 # Runtime
-export NCCL_VERSION=2.4.2
+export NV_CUDA_LIB_VERSION=11.1.1-1
+
+export NV_NVTX_VERSION=11.1.74-1
+export NV_LIBNPP_VERSION=11.1.2.301-1
+export NV_LIBNPP_PACKAGE=libnpp-11-1=${NV_LIBNPP_VERSION}
+export NV_LIBCUSPARSE_VERSION=11.3.0.10-1
+
+export NV_LIBCUBLAS_PACKAGE_NAME=libcublas-11-1
+export NV_LIBCUBLAS_VERSION=11.3.0.106-1
+export NV_LIBCUBLAS_PACKAGE=${NV_LIBCUBLAS_PACKAGE_NAME}=${NV_LIBCUBLAS_VERSION}
+
+export NV_LIBNCCL_PACKAGE_NAME=libnccl2
+export NV_LIBNCCL_PACKAGE_VERSION=2.8.4-1
+export NCCL_VERSION=2.8.4-1
+export NV_LIBNCCL_PACKAGE=${NV_LIBNCCL_PACKAGE_NAME}=${NV_LIBNCCL_PACKAGE_VERSION}+cuda11.1
 
 apt-get update && apt-get install -y --no-install-recommends \
-        cuda-libraries-$CUDA_PKG_VERSION \
-        cuda-nvtx-$CUDA_PKG_VERSION \
-        libnccl2=$NCCL_VERSION-1+cuda10.0 && \
-    apt-mark hold libnccl2 && \
-    rm -rf /var/lib/apt/lists/*
+    cuda-libraries-11-1=${NV_CUDA_LIB_VERSION} \
+    ${NV_LIBNPP_PACKAGE} \
+    cuda-nvtx-11-1=${NV_NVTX_VERSION} \
+    libcusparse-11-1=${NV_LIBCUSPARSE_VERSION} \
+    ${NV_LIBCUBLAS_PACKAGE} \
+    ${NV_LIBNCCL_PACKAGE} \
+    && rm -rf /var/lib/apt/lists/*
+
+# Keep apt from auto upgrading the cublas and nccl packages. See https://gitlab.com/nvidia/container-images/cuda/-/issues/88
+apt-mark hold ${NV_LIBCUBLAS_PACKAGE_NAME} ${NV_LIBNCCL_PACKAGE_NAME}
 
 # Devel
-apt-get update && apt-get install -y --no-install-recommends \
-        cuda-libraries-dev-$CUDA_PKG_VERSION \
-        cuda-nvml-dev-$CUDA_PKG_VERSION \
-        cuda-minimal-build-$CUDA_PKG_VERSION \
-        cuda-command-line-tools-$CUDA_PKG_VERSION \
-        libnccl-dev=$NCCL_VERSION-1+cuda10.0 && \
-    rm -rf /var/lib/apt/lists/*
+export NV_CUDA_LIB_VERSION="11.1.1-1"
 
-export LIBRARY_PATH=/usr/local/cuda/lib64/stubs
+export NV_CUDA_CUDART_DEV_VERSION=11.1.74-1
+export NV_NVML_DEV_VERSION=11.1.74-1
+export NV_LIBCUSPARSE_DEV_VERSION=11.3.0.10-1
+export NV_LIBNPP_DEV_VERSION=11.1.2.301-1
+export NV_LIBNPP_DEV_PACKAGE=libnpp-dev-11-1=${NV_LIBNPP_DEV_VERSION}
+
+export NV_LIBCUBLAS_DEV_VERSION=11.3.0.106-1
+export NV_LIBCUBLAS_DEV_PACKAGE_NAME=libcublas-dev-11-1
+export NV_LIBCUBLAS_DEV_PACKAGE=${NV_LIBCUBLAS_DEV_PACKAGE_NAME}=${NV_LIBCUBLAS_DEV_VERSION}
+
+export NV_LIBNCCL_DEV_PACKAGE_NAME=libnccl-dev
+export NV_LIBNCCL_DEV_PACKAGE_VERSION=2.8.4-1
+export NCCL_VERSION=2.8.4-1
+export NV_LIBNCCL_DEV_PACKAGE=${NV_LIBNCCL_DEV_PACKAGE_NAME}=${NV_LIBNCCL_DEV_PACKAGE_VERSION}+cuda11.1
+
+ apt-get update && apt-get install -y --no-install-recommends \
+    libtinfo5 libncursesw5 \
+    cuda-cudart-dev-11-1=${NV_CUDA_CUDART_DEV_VERSION} \
+    cuda-command-line-tools-11-1=${NV_CUDA_LIB_VERSION} \
+    cuda-minimal-build-11-1=${NV_CUDA_LIB_VERSION} \
+    cuda-libraries-dev-11-1=${NV_CUDA_LIB_VERSION} \
+    cuda-nvml-dev-11-1=${NV_NVML_DEV_VERSION} \
+    ${NV_LIBNPP_DEV_PACKAGE} \
+    libcusparse-dev-11-1=${NV_LIBCUSPARSE_DEV_VERSION} \
+    ${NV_LIBCUBLAS_DEV_PACKAGE} \
+    ${NV_LIBNCCL_DEV_PACKAGE} \
+    && rm -rf /var/lib/apt/lists/*
+
+# Keep apt from auto upgrading the cublas and nccl packages. See https://gitlab.com/nvidia/container-images/cuda/-/issues/88
+RUN apt-mark hold ${NV_LIBCUBLAS_DEV_PACKAGE_NAME} ${NV_LIBNCCL_DEV_PACKAGE_NAME}
